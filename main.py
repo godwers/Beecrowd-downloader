fea72ae4978b4402efb4ca256a0fe26e18303783
godwers <gondes.historia@gmail.com>
2025-12-12 09:58:44 -0300

    added main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from time import sleep
+from modules.login import login
+from modules.question_list import get_solved_list
+from modules.repository import create_repository
+from modules.constants import LANGUAGE_ID
+
+
+def main() -> None:
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--headless=new")  # Comment this line to debug
+    driver = webdriver.Firefox(options=options)
+
+    login(driver)
+
+    print("Login Successfully")
+
+    solved_list = get_solved_list(driver)
+    print("Accepted answers was retrived")
+    print(solved_list)
+
+    # create_repository()
+
+    for language in solved_list:
+        pointer = 0
+        questions_list = solved_list[language]
+        while pointer < len(questions_list):
+            answer_url = f"https://judge.beecrowd.com/pt/runs?problem_id={questions_list[pointer]}&answer_id=1&language_id={LANGUAGE_ID[language]}"
+            print(answer_url)
+            driver.get(answer_url)
+            break
+
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
