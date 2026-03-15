83a0a653f8291e73c484d0199ca7a959d22304a5
godwers <68791933+godwers@users.noreply.github.com>
2026-03-15 15:55:28 -0300

    this implementation sucks ass 😭

--- modules/answers_list.py
+++ modules/answers_list.py
@@ -1,11 +1,9 @@
 from time import sleep
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
-from .constants import ACCEPTED_LIST_URL
+from .constants import HOME_URL,LANGUAGE_ID,ACCEPTED_LIST_URL
 
 
 def _get_lastpage(driver, xpath: str) -> str | None:
@@ -29,6 +27,11 @@ def _get_page_number(driver) -> int | None:
 
     return int(last_page)
 
+def _get_unique_code(driver,code_number : str,language : str) -> str:
+    driver.get(f"{HOME_URL}/runs?problem_id={code_number}&answer_id=1&language_id={LANGUAGE_ID[language]}")
+    unique_code = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[2]/div[4]/div/div[2]/table/tbody/tr[1]/td[1]/a").text
+    driver.back()
+    return unique_code
 
 def _list_loop(driver) -> dict[str, dict[str, str]]:
     questions_list = dict()
@@ -40,14 +43,9 @@ def _list_loop(driver) -> dict[str, dict[str, str]]:
     pagina = 1
     while pagina <= pagina_final:
         driver.get(f"{ACCEPTED_LIST_URL}&page={pagina}&sort=problem_id&direction=asc")
-        sleep(5)
+        sleep(1)
         try:
             for i in range(1, 29):
-                codigo_unico = driver.find_element(
-                    By.XPATH,
-                    f"/html/body/div[7]/div[2]/div[2]/div[4]/div/div[2]/table/tbody/tr[{i}]/td[1]",
-                ).text
-
                 numeros = driver.find_element(
                     By.XPATH,
                     f"/html/body/div/div/div[2]/div[4]/div/div[2]/table/tbody/tr[{i}]/td[3]/a",
@@ -57,6 +55,9 @@ def _list_loop(driver) -> dict[str, dict[str, str]]:
                     By.XPATH,
                     f"/html/body/div[7]/div/div[2]/div[4]/div/div[2]/table/tbody/tr[{i}]/td[6]",
                 ).text
+
+                codigo_unico = _get_unique_code(driver,numeros,linguagem)
+                print(codigo_unico)
 
                 if linguagem not in questions_list:
                     questions_list[linguagem] = dict()
