ecc20f8734eaa0f71436d3ce8710cb53fd926b69
godwers <68791933+godwers@users.noreply.github.com>
2026-03-15 15:55:28 -0300

    now it doenst download copies of the extension

--- modules/addons.py
+++ modules/addons.py
@@ -1,16 +1,23 @@
-from os import getcwd,remove
+import os 
 
-from wget import download
+from wget import download,filename_from_url
 
 from .constants import UBLOCK_ORIGIN_URL
 
-def _get_ublock_addon() -> str:
-    return download(UBLOCK_ORIGIN_URL,bar=None,out=getcwd())
+
+def _get_file_name() -> str:
+    filename = filename_from_url(UBLOCK_ORIGIN_URL)
+    return os.path.join(os.getcwd(),filename)
+
+def _get_ublock_addon() -> None:
+    download(UBLOCK_ORIGIN_URL,bar=None,out=os.getcwd())
 
 def add_ublock(driver):
-    path_file = _get_ublock_addon()
+    path_file = _get_file_name()
+    if not os.path.exists(path_file):
+        _get_ublock_addon()
     driver.install_addon(path_file)
     return path_file
 
 def remove_ublock(path_file):
-    remove(path_file)
+    os.remove(path_file)
